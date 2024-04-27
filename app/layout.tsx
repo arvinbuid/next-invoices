b127d26bfcd27c3b277d6246a5bed7a6252423ec
Arvin Buid <arvinbuid01@gmail.com>
2024-04-27 14:48:33 +0800

    added fonts, styles, hero-picture using next Image, suppressHydrationWarning={true} into layout component, and images: {unoptimized: true} prop inside next.config.js

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
 
 export default function RootLayout({
   children,
@@ -7,7 +8,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body suppressHydrationWarning={true}>{children}</body>
+      <body
+        suppressHydrationWarning={true}
+        className={`${inter.className}
+        antialiased`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
