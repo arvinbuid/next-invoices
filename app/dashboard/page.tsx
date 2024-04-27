dcc10a636f32907990b2ad3ff12b6cccd944f357
Arvin Buid <arvinbuid01@gmail.com>
2024-04-27 15:04:37 +0800

    added customers and invoices folder with page.tsx file as a new route page inside the app/dashboard.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,9 @@
 export default function Page() {
   return (
-    <h1 className="mt-10 text-center text-4xl font-semibold">Dashboard Page</h1>
+    <div>
+      <h1 className="mt-10 text-center text-4xl font-semibold">
+        Dashboard Page
+      </h1>
+    </div>
   );
 }
