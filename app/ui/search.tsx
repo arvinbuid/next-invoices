49be70775afd63616d553330c69063bada3ff3e3
Arvin Buid <arvinbuid01@gmail.com>
2024-04-29 17:39:15 +0800

    Imported fetchInvoicesPages inside app/dashboard/invoices/page.tsx to query from database directly inside the component. Create a pagination using URL search parameters and usePathname.  Implement a reset page to number 1 when a user type a new query using params.set('page', '1')

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -10,10 +10,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
 
   const handleSearch = useDebouncedCallback((term) => {
-    console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
 
-    // if input is empty, delete it
     if (term) {
       params.set('query', term);
     } else {
