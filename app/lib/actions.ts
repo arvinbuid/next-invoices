c049f7eee194c34828eaa3e8d727420ac012ab44
Arvin Buid <arvinbuid01@gmail.com>
2024-04-30 17:10:17 +0800

    Created a dynamic route segment with invoice id. Implement CRUD actions inside app/lib/actions.ts. Implement delete inside app/ui/invoices/buttons.tsx. Implement update inside edit-form.tsx.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,6 +5,9 @@ import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
+// ACTIONS
+
+// Type annotation using Zod
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string(),
@@ -13,8 +16,10 @@ const FormSchema = z.object({
   date: z.string(),
 });
 
+// Use Zod to create the expected types
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
+// Create invoice
 export async function createInvoice(formData: FormData) {
   const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
@@ -32,4 +37,33 @@ export async function createInvoice(formData: FormData) {
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
+}
+
+// Use Zod to update the expected types
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+// Update invoice
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+// Delete invoice
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
 }
--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
+import { updateInvoice } from '@/app/lib/actions';
 import {
   CheckIcon,
   ClockIcon,
@@ -17,8 +18,10 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+
   return (
-    <form>
+    <form action={updateInvoiceWithId}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -109,6 +112,7 @@ export default function EditInvoiceForm({
           </div>
         </fieldset>
       </div>
+      {/* Cancel/Edit Invoice */}
       <div className="mt-6 flex justify-end gap-4">
         <Link
           href="/dashboard/invoices"
