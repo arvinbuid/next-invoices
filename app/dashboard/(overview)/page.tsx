2c5c32b93f93f1dc9c1f3e04e940370365ae3041
Arvin Buid <arvinbuid01@gmail.com>
2024-05-01 17:40:13 +0800

    Moved favicon.ico and opengraph-image.png into the app/ folder. Created metadata structure inside app/layout.tsx. Created metadata title inside Dashboard, Customers Page, Invoices Page, Edit and Create Invoice.

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,6 +2,12 @@ import CardWrapper from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+// metadata for SEO
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 
 import { Suspense } from 'react';
 import {
--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from 'next';
+
+// metadata for SEO
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
 export default function Customers() {
   return <p>Customers Page</p>;
 }
